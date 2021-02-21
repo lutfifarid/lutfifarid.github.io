d51078d70a3b2d39267eab4ab2a7b3be13f49488
lutfi <lutfifarid320@gmail.com>
2021-02-21 18:10:27 +0700

    update information

--- js/home.js
+++ js/home.js
@@ -4,14 +4,14 @@
 // Cool loopy loop of adjectives I like :)
 $(document).ready(function() {
 
-var items = ["family guardian", "SQL-caster", "data enthusiast", "insight seeker", "code explorer"],
+var items = ["young daddy", "SQL-caster", "data enthusiast", "insight seeker", "code explorer"],
     $text = $( '#about-me span' ),
     delay = 2; //seconds
 
 function loop ( delay ) {
 
     $.each( items, function ( i, elm ){
-        console.log("aa")
+        // console.log("items")
         $text.delay( delay*1E3).fadeOut();
         $text.queue(function(){
             $text.html( items[i] );
